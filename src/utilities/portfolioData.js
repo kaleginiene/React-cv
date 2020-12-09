ee294259c840b9c322797c87e99981191bcf15f6
kaleginiene <ieva.kaleginiene@gmail.com>
2020-12-09 21:52:50 +0200

    minor ssl change

--- src/utilities/portfolioData.js
+++ src/utilities/portfolioData.js
@@ -12,7 +12,7 @@ const portfolioData = [
     id: 1,
     caption: "WineStock system",
     url: WineStock,
-    page: "http://wine-system.uzprogramuosiu-viska.lt/",
+    page: "https://wine-system.uzprogramuosiu-viska.lt/",
     git: "https://github.com/kaleginiene/wine-system-frontend",
     backend: "https://github.com/kaleginiene/wine-system-backend",
   },
