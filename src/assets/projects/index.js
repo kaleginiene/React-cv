31d0d5ec4180be3a66193d2979767f1d1a79e0b6
kaleginiene <ieva.kaleginiene@gmail.com>
2021-01-04 15:52:54 +0200

    added a recipes web to the portfolio page

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,2 +1,3 @@
 export { default as StaticCV } from "../../assets/projects/static-cv.JPG";
 export { default as WineStock } from "../../assets/projects/vineStock.JPG";
+export { default as RecipesWeb } from "../../assets/projects/recipes.JPG";
--- src/utilities/portfolioData.js
+++ src/utilities/portfolioData.js
@@ -1,15 +1,23 @@
-import { StaticCV, WineStock } from "../assets/projects";
+import { StaticCV, WineStock, RecipesWeb } from "../assets/projects";
 
 const portfolioData = [
   {
     id: 1,
+    caption: "Recipes website",
+    url: RecipesWeb,
+    page: "http://recipes.uzprogramuosiu-viska.lt/",
+    git: "https://github.com/kaleginiene/recipes-website.git",
+    backend: "https://github.com/kaleginiene/recipes-website-backend.git",
+  },
+  {
+    id: 2,
     caption: "Static-CV",
     url: StaticCV,
     page: "http://resume.uzprogramuosiu-viska.lt/",
     git: "https://github.com/kaleginiene/CV",
   },
   {
-    id: 1,
+    id: 3,
     caption: "WineStock system",
     url: WineStock,
     page: "https://wine-system.uzprogramuosiu-viska.lt/",
